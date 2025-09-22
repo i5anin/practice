1cd79498562e8ad7abcf4ca95383afe7cef00bfa
Isanin Sergey <sergey.isanin+github@gmail.com>
2025-09-22 18:05:22 +0300

    2. Настройка проекта. Иконочный шрифт

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ export default function App() {
     return (
         <Div className="app">
             <div>123</div>
-
+            <i className="fa fa-home"></i>
+            <i className="fa fa-user"></i>
         </Div>
     )
 }
