2f80c15984afa7911743c32ab92eefe8262cf539
Isanin Sergey <sergey.isanin+github@gmail.com>
2025-09-26 19:40:34 +0300

    8. Первичная реализация BFF. Регистрация

--- src/bff/bff.js
+++ src/bff/bff.js
@@ -1,3 +1,10 @@
+const generateDate = () =>
+    new Date(Math.random() * 1000000000000 + 1999999999999)
+        .toISOString()
+        .substring(0, 16)
+        .replace('T', ' ');
+
+
 export const server = {
     async authorize(authLogin, authPassword) {
         const response = await fetch('http://localhost:3005/users');
@@ -20,6 +27,7 @@ export const server = {
         }
 
         const session = {
+            user: {login: user.login, role_id: user.role_id},
             logout() {
                 Object.keys(session).forEach((key) => {
                     delete session[key];
@@ -33,6 +41,38 @@ export const server = {
         return {
             error: null,
             res: session,
+        };
+    },
+
+    async register(regLogin, regPassword) {
+        const response = await fetch('http://localhost:3005/users');
+        const users = await response.json();
+
+        const user = users.find(({login}) => login === regLogin);
+
+        if (user) {
+            return {
+                error: 'Такой логин уже занят',
+                res: null,
+            };
+        }
+
+        const newUser = {
+            login: regLogin,
+            password: regPassword,
+            registed_at: generateDate(), //new Date().toISOString(), // дата регистрации
+            role_id: 2                                               // базовая роль
+        };
+
+        await fetch('http://localhost:3005/users', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json; charset=utf-8'},
+            body: JSON.stringify(newUser),
+        });
+
+        return {
+            error: null,
+            res: newUser,
         };
     }
 };
