d1d100a7fec822ddb4cad67ce132a64b7e01e307
Isanin Sergey <sergey.isanin+github@gmail.com>
2025-10-02 19:37:19 +0300

    10. Основной компонент блога

---
+++ src/Blog.jsx
@@ -0,0 +1,42 @@
+import { Routes, Route, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Content = styled.div`
+    text-align: center;
+`;
+
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Футер</div>;
+
+const HomePage = () => <div>Главная страница</div>;
+const LoginPage = () => <div>Вход</div>;
+const RegisterPage = () => <div>Регистрация</div>;
+const UsersPage = () => <div>Пользователи</div>;
+const NewPostPage = () => <div>Новая статья</div>;
+const NotFoundPage = () => <div>Ошибка: страница не найдена</div>;
+
+const PostPage = () => {
+    const { postId } = useParams();
+    return <div>Статья {postId}</div>;
+};
+
+export default function Blog() {
+    return (
+        <>
+            <Header />
+            <Content>
+                <h2>Контент страницы</h2>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/users" element={<UsersPage />} />
+                    <Route path="/post" element={<NewPostPage />} />
+                    <Route path="/post/:postId" element={<PostPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </Content>
+            <Footer />
+        </>
+    );
+}
